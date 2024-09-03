d8f176fff28d73a4f77be0c950b308c48d20a4bd
Mario Mora <marioemorac@gmail.com>
2024-09-03 22:57:23 +0200

    Creado el sitio de contacto y centrado el navbar

--- src/componentes/Contacto.tsx
+++ src/componentes/Contacto.tsx
@@ -2,18 +2,18 @@ import React from "react";
 
 const Contacto: React.FC = () =>{
     return(
-        <div>
-            <div>
-                <a href="https://www.facebook.com/IglesiaBautistaMasVida1">Iglesia Bautista Mas Vida</a>
+        <div className="contacto_container">
+            <div className="social_container">
+                <a href="https://www.facebook.com/IglesiaBautistaMasVida1" target="_blank"><img className="social" src="/img/ibmv_fb.png" alt="" /><br/>Iglesia Bautista Más Vida</a>
             </div>
-            <div>
-                <a href="https://www.instagram.com/masvida.alcorcon">Iglesia Bautista Mas Vida</a>
+            <div className="social_container">
+                <a href="https://www.instagram.com/masvida.alcorcon" target="_blank"><img className="social" src="/img/ibmv_ig.png" alt="" /><br/>Iglesia Bautista Más Vida</a>
             </div>
-            <div>
-                <a href="mailto:pastormeyermasvida@icloud.com">Iglesia Bautista Mas Vida</a>
+            <div className="social_container">
+                <a href="mailto:pastormeyermasvida@icloud.com" target="_blank"><img className="social" src="/img/ibmv_email.png" alt="" /><br/>pastormeyermasvida@icloud.com</a>
             </div>
-            <div>
-                <a href="https://wa.me/34671125688"><img src="../img/ibmv_fb.png" alt="" /></a>
+            <div className="social_container">
+                <a href="https://wa.me/34671125688" target="_blank"><img className="social" src="/img/ibmv_wa.png" alt="" /><br/>671125688</a>
             </div>
         </div>
     );
