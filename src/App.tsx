890e528749d5263422f454e22b6cd11ffdf46cbe
Mario Mora <marioemorac@gmail.com>
2024-09-09 00:02:07 +0200

    Cambio de fondo y mejora del navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,72 +1,86 @@
-import React, { useRef } from 'react';
-import './App.css';
-import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
-import Inicio from './componentes/Inicio';
-import Media from './componentes/Media';
-import Contacto from './componentes/Contacto';
-import Ministerios from './componentes/Ministerios';
-import Visita from './componentes/Visita';
+import React, { useRef } from "react";
+import { BrowserRouter as Router, Route, Link, Routes } from "react-router-dom";
+import Inicio from "./componentes/Inicio";
+import Media from "./componentes/Media";
+import Visita from "./componentes/Visita";
+import Contacto from "./componentes/Contacto";
+import Ministerios from "./componentes/Ministerios";
 
-const App: React.FC = () =>{
-
+const App: React.FC = () => {
   const collapseRef = useRef<HTMLDivElement | null>(null);
 
   const handleNavItemClick = () => {
     const collapseElement = collapseRef.current;
-    if (collapseElement && collapseElement.classList.contains('show')) {
-      const bsCollapse = new (window as any).bootstrap.Collapse(collapseElement, {
-        toggle: true,
-      });
-      bsCollapse.hide();
+    if (collapseElement && collapseElement.classList.contains("show")) {
+      const bsCollapse = new (window as any).bootstrap.Collapse(collapseElement);
+      bsCollapse.hide(); // Oculta el menú colapsable
     }
   };
 
-  return(
+  return (
     <Router>
-
       <div>
         <nav className="navbar navbar-expand-lg navbar-light bg-light">
           <div className="container">
             <a className="navbar-brand" href="#">
-              <img className='social' src="/img/ibmv_logo.png" alt="Logo" />
+              <img className="social" src="/img/ibmv_logo.png" alt="Logo" />
             </a>
-            <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <button
+              className="navbar-toggler"
+              type="button"
+              data-bs-toggle="collapse"
+              data-bs-target="#navbarSupportedContent"
+              aria-controls="navbarSupportedContent"
+              aria-expanded="false"
+              aria-label="Toggle navigation"
+            >
               <span className="navbar-toggler-icon"></span>
             </button>
-            <div className="collapse navbar-collapse justify-content-center" id="navbarSupportedContent">
+            <div
+              className="collapse navbar-collapse justify-content-center"
+              id="navbarSupportedContent"
+              ref={collapseRef} // Aseguramos que el ref está conectado
+            >
               <ul className="navbar-nav">
                 <li className="nav-item">
-                  <Link className='nav-link active' to="/" onClick={handleNavItemClick}>Inicio</Link>
+                  <Link className="nav-link active" to="/" onClick={handleNavItemClick}>
+                    Inicio
+                  </Link>
                 </li>
                 <li className="nav-item">
-                  <Link className='nav-link active' to="/visita" onClick={handleNavItemClick}>Visítanos</Link>
+                  <Link className="nav-link active" to="/visita" onClick={handleNavItemClick}>
+                    Visítanos
+                  </Link>
                 </li>
                 <li className="nav-item">
-                  <Link className='nav-link active' to="/ministerios" onClick={handleNavItemClick}>Ministerios</Link>
+                  <Link className="nav-link active" to="/ministerios" onClick={handleNavItemClick}>
+                    Ministerios
+                  </Link>
                 </li>
                 <li className="nav-item">
-                  <Link className='nav-link active' to="/media" onClick={handleNavItemClick}>Media</Link>
+                  <Link className="nav-link active" to="/media" onClick={handleNavItemClick}>
+                    Media
+                  </Link>
                 </li>
                 <li className="nav-item">
-                  <Link className='nav-link active' to="/contacto" onClick={handleNavItemClick}>Contacto</Link>
+                  <Link className="nav-link active" to="/contacto" onClick={handleNavItemClick}>
+                    Contacto
+                  </Link>
                 </li>
               </ul>
             </div>
           </div>
         </nav>
         <Routes>
-          <Route path="/" element={<Inicio />}></Route>
-          <Route path="/media" element={<Media />}></Route>
-          <Route path="/visita" element={<Visita />}></Route>
-          <Route path="/contacto" element={<Contacto />}></Route>
-          <Route path="/ministerios" element={<Ministerios />}></Route>
+          <Route path="/" element={<Inicio />} />
+          <Route path="/media" element={<Media />} />
+          <Route path="/visita" element={<Visita />} />
+          <Route path="/contacto" element={<Contacto />} />
+          <Route path="/ministerios" element={<Ministerios />} />
         </Routes>
-
       </div>
-
-
     </Router>
   );
-}
+};
 
 export default App;
--- src/componentes/inicio.tsx
+++ src/componentes/inicio.tsx
@@ -2,7 +2,9 @@ import React from "react";
 
 const Inicio: React.FC = () =>{
     return(
+        <div className="container">
         <h2>Hello!</h2>
+        </div>
     );
 }
 
