7aa340f7e8ed538a5392a6e44086e5523fe27d6d
Mario Mora <marioemorac@gmail.com>
2024-09-09 00:35:39 +0200

    Agregando la sección de Media

--- src/componentes/Media.tsx
+++ src/componentes/Media.tsx
@@ -2,7 +2,18 @@ import React from "react";
 
 const Media: React.FC = () =>{
     return(
-        <h2>Media</h2>
+        <div>
+            <div className="titulo"> 
+                <h2>Enseñanzas</h2>
+            </div>
+            <div className="container">
+                <iframe width="560" height="315" src="https://www.youtube.com/embed/NNLKt2zs51k?si=ZS0r7aOEiKclI9n6" 
+                title="YouTube video player" frameBorder="0" 
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
+                referrerPolicy="strict-origin-when-cross-origin" allowFullScreen></iframe>            
+            </div>
+            
+        </div>
     );
 }
 
