52b947a64e4ff438293bd5e62de9275ef5a45ba3
Mario Mora <marioemorac@gmail.com>
2024-09-10 11:56:55 +0200

    Primeros elementos del componente inicio

--- src/componentes/inicio.tsx
+++ src/componentes/inicio.tsx
@@ -2,8 +2,14 @@ import React from "react";
 
 const Inicio: React.FC = () =>{
     return(
-        <div className="container">
-        <h2>Hello!</h2>
+        <div>
+            <div className="titulo"> 
+                <h2>Somos la Iglesia Bautista Más Vida</h2>
+            </div>
+            <div className="container">
+                    Somos una congregación que busca ser bíblica en todo lo que hace, ofreciendo una claridad teológica para conocer mejor a Dios. Nos esmeramos en vivir el modelo del Nuevo Testamento, de una iglesia que no solo es definida por su claridad teológica, sino también por su amor, calidez, amabilidad, y estar verdaderamente preocupados los unos por los otros. En esta iglesia encontrarás una congregación amorosa, donde buscamos amar a Dios y a los demás. 
+            </div>
+            
         </div>
     );
 }
