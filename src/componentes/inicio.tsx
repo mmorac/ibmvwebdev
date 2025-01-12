fd69c0e9befaaeeb82fe787c8ee3742cf9322b93
Mario Mora <marioemorac@gmail.com>
2025-01-12 19:40:56 +0100

    Agregando configuración CI/CD
    Agregando configuración para Firebase y CircleCI

--- src/componentes/inicio.tsx
+++ src/componentes/inicio.tsx
@@ -7,7 +7,9 @@ const Inicio: React.FC = () =>{
                 <h2>Somos la Iglesia Bautista Más Vida</h2>
             </div>
             <div className="container">
+                <p className="descripcion">
                     Somos una congregación que busca ser bíblica en todo lo que hace, ofreciendo una claridad teológica para conocer mejor a Dios. Nos esmeramos en vivir el modelo del Nuevo Testamento, de una iglesia que no solo es definida por su claridad teológica, sino también por su amor, calidez, amabilidad, y estar verdaderamente preocupados los unos por los otros. En esta iglesia encontrarás una congregación amorosa, donde buscamos amar a Dios y a los demás. 
+                </p>
             </div>
             
         </div>
