2d0637ac7c20a48a79eefe9cc474d4436bbc9089
Mario Mora <marioemorac@gmail.com>
2025-01-16 00:12:49 +0100

    Ajustes a ministerios

--- src/componentes/Ministerios.tsx
+++ src/componentes/Ministerios.tsx
@@ -2,8 +2,20 @@ import React from "react";
 
 const Ministerios: React.FC = () =>{
     return(
-        <div className="titulo">
-            <h2>Ministerios</h2>
+        <div>
+            <div className="titulo">
+                <h2>Ministerios</h2>
+            </div>
+            <div className="contenido">
+                <div className="ministerio_ninos">
+                    <p>
+                        Niños
+                    </p>
+                </div>
+                <div className="ministerio_jovenes">Jóvenes</div>
+                <div className="ministerio_adultos">Adultos</div>
+                <div className="ministerio_misiones">Misiones</div>
+            </div>
         </div>
     );
 }
