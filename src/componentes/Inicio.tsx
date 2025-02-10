03209f5e20f252f555af132ae833924b3366da8e
Mario Mora <marioemorac@gmail.com>
2025-02-10 17:19:01 +0100

    Ministerios e Inicio
    Se mejoró la página de ministerios
    
    Se agregaron botones al inicio que redirigen a la información de visitantes

--- src/componentes/Inicio.tsx
+++ src/componentes/Inicio.tsx
@@ -1,7 +1,8 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
-const Inicio: React.FC = () =>{
-    return(
+const Inicio: React.FC = () => {
+    return (
         <div>
             <div className="titulo"> 
                 <h2>Somos la Iglesia Bautista Más Vida</h2>
@@ -11,7 +12,10 @@ const Inicio: React.FC = () =>{
                     Somos una congregación que busca ser bíblica en todo lo que hace, ofreciendo una claridad teológica para conocer mejor a Dios. Nos esmeramos en vivir el modelo del Nuevo Testamento, de una iglesia que no solo es definida por su claridad teológica, sino también por su amor, calidez, amabilidad, y estar verdaderamente preocupados los unos por los otros. En esta iglesia encontrarás una congregación amorosa, donde buscamos amar a Dios y a los demás. 
                 </p>
             </div>
-            
+            <div className="accesosRapidos">
+                <Link to="/visita" className="botonInicio">¿Primera visita?</Link>
+                <Link to="/visita" className="botonInicio">¿Qué creemos?</Link>
+            </div>
         </div>
     );
 }
--- src/componentes/Visita.tsx
+++ src/componentes/Visita.tsx
@@ -1,12 +1,31 @@
 import React from "react";
 
-const Visita: React.FC = () =>{
-    return(
+const Visita: React.FC = () => {
+    return (
         <div>
-            <div className="titulo"> 
-                <h2>Visítanos</h2>
+            <div className="titulo">
+                <h1>Visítanos</h1>
             </div>
+            <div></div>
             <div className="direcciones">
+                <div className="subtituloVisita">
+                    <h3 className="vertical-align">¿Cuándo?</h3>
+                </div>
+                <p>
+                    Culto de oración - Miércoles 19:30.
+                </p>
+                <p>
+                    Cultos generales - Domingo 10:00 y 12:30
+                </p>
+                <p>
+                    Escuela dominical - Domingo 11:15
+                </p>
+                <p>
+                    Culto de oración - Miércoles 19:30
+                </p>
+                <div className="subtituloVisita">
+                    <h3 className="vertical-align">¿Dónde?</h3>
+                </div>
                 <p>
                     Estamos en la calle Sahagún 28 en Alcorcón, con acceso cercano desde RENFE, autobús y Metro.
                 </p>
